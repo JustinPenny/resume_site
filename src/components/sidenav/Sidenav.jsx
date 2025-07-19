1100418f3ac18fb49af02ac9a6237ed0b7958022
Justin Penny <justincpenny@hotmail.com>
2025-07-18 22:44:22 -0400

    working on nav bar for mobile

--- src/components/sidenav/Sidenav.jsx
+++ src/components/sidenav/Sidenav.jsx
@@ -1,43 +1,70 @@
 import './sidenav.css'
 import profile from '../../assets/profile.jpg'
+import { useState } from 'react'
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri'
 
-const sidenav = () =>{
+const Menu = () =>(
+    <>
+    <li className='nav-item'>
+        <a href="#about">About</a>
+    </li>
+    <li className='nav-item'>
+        <a href="#education">Education</a>
+    </li>
+    <li className='nav-item'>
+        <a href="#experience">Experience</a>
+    </li>
+    <li className='nav-item'>
+        <a href="#skills">Skills</a>
+    </li>
+    <li className='nav-item'>
+        <a href="#projects">Projects</a>
+    </li>
+    <li className='nav-item'>
+        <a href="#interests">Interests</a>
+    </li>
+    <li className='nav-item'>
+        <a href="#info">Info</a>
+    </li>
+    </>
+)
+
+const Sidenav = () =>{
+    const [toggleMenu, setToggleMenu] = useState(false);
     return(
         <div>
-            <div className='sidenav gradient__bg'>
-                <div className='sidenav__image'>
+            <div className='Sidenav gradient__bg'>
+                <div className='Sidenav__image'>
                     <a href="#about">
                         <img src={profile} alt="profile"/>
                     </a>
                 </div>
-                
+                <ul className='Sidenav__list'>
+                    <Menu />
+                </ul>
+        
+        
+                <div className='Sidebar__nav--toggle'>
+                {toggleMenu
+                ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)}/>
+                : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)}/>
+                }
 
-                    <ul className='sidenav_list'>
-                        <li className='nav-item'>
-                            <a href="#about">About</a>
-                        </li>
-                        <li className='nav-item'>
-                            <a href="#education">Education</a>
-                        </li>
-                        <li className='nav-item'>
-                            <a href="#experience">Experience</a>
-                        </li>
-                        <li className='nav-item'>
-                            <a href="#skills">Skills</a>
-                        </li>
-                        <li className='nav-item'>
-                            <a href="#projects">Projects</a>
-                        </li>
-                        <li className='nav-item'>
-                            <a href="#interests">Interests</a>
-                        </li>
-                        <li className='nav-item'>
-                            <a href="#info">Info</a>
-                        </li>
-                    </ul>
+                {/*toggleMenu && -> if menu is toggled and the variable is true, execute block */}
+                {toggleMenu && (
+                <div className="content__navbar-menu_container scale-up-center">
+                    <div className="content__navbar-menu_container-links">
+                    <Menu />
+                    </div>
+                </div>
+                )
+                }
+                </div>
+
+
             </div>
         </div>
     )
 }
 
-export default sidenav
+export default Sidenav
